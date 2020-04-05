1fddeea7901ce88fd51aec3116a8bce3cf5faf78
Cristian Ayub <crisaych@gmail.com>
2020-04-04 18:15:28 -0600

    Simulated annealing algorithm completed

--- SearchAlgorithm.java
+++ SearchAlgorithm.java
@@ -1,7 +1,23 @@
+
+/**
+ * Authors: Miguel A. Zamudio & Cristian Ayub
+ * Class: 4320/5314
+ * Instructor: Dr. Christopher Kiekintveld
+ * Last updated: 04/04/2020
+ */
+
 import java.util.*;
 
 public class SearchAlgorithm {
 
+  /**
+   * This method allows to run simulated annealing algorithm given
+   * a problem and a deadline
+   * 
+   * @param problem
+   * @param deadline
+   * @return
+   */
   public Schedule simulatedAnnealingAlg(SchedulingProblem problem, long deadline) {
     Schedule solution = problem.getEmptySchedule();
     boolean[] assignment = new boolean[problem.courses.size()];
@@ -11,14 +27,26 @@ public class SearchAlgorithm {
     return solution;
   }
 
+  /**
+   * This method implements simulated annealing algorithm
+   * given a problem, a schedule, a list of already assigned courses, 
+   * and a deadline
+   * 
+   * @param problem
+   * @param currentSolution
+   * @param assignment
+   * @param deadline
+   * @return
+   */
   public Schedule iterativeSA(SchedulingProblem problem, Schedule currentSolution, boolean[] assignment, long deadline) {
     int time = 100;
-    double tmax = 5000;
-    double tmin = 10;
-    double coolingRate = 0.5;
+    double tmax = getTemperature(currentSolution);
+    double tmin = 0;
+    double coolingRate = 0.3;
 
-    currentSolution = naiveBaseline(problem, deadline);
-    // currentSolution = initialSchedule(problem, currentSolution, assignment);
+    // currentSolution = naiveBaseline(problem, deadline);
+    // currentSolution = initialSchedule(problem, currentSolution);
+    currentSolution = backtrackAlg(problem, deadline);
 
     for(int i = 0; i < time; i++) {
       if(i % 10 == 0) tmax *= coolingRate;
@@ -31,87 +59,94 @@ public class SearchAlgorithm {
       else{
         if(probabilityMet(deltaE,tmax)) currentSolution = nextSolution;
       }
-      System.out.println(i);
     }
     return currentSolution;
   }
 
-  int pickRandomCourse(SchedulingProblem problem, boolean[] assignment) {
-    boolean picked = false;
-    Random random = new Random();
+  /**
+   * This method calculates the cooling rate for simulated annealing
+   * depending on the size of the given schedule
+   * 
+   * @param schedule
+   * @return
+   */
+  double getCoolingRate(Schedule schedule) {
+    double initialRate = 0.3;
 
-    while(!picked) {
-      int courseIndex = random.nextInt(problem.courses.size());
-      if(!assignment[courseIndex]) {
-        assignment[courseIndex] = true;
-        return courseIndex;
-      }
+    int size = schedule.schedule.length;
+
+    if (size < 100) {
+      initialRate += 0;
+    }
+    if (size < 1000) {
+      initialRate += 0.1;
+    }
+    if (size < 10000) {
+      initialRate += 0.1;
     }
 
-    return -1;
+    return initialRate;
   }
 
+  /**
+   * This method calculates the temperature depending on the 
+   * size of the given schedule
+   * 
+   * @param schedule
+   * @return
+   */
+  double getTemperature(Schedule schedule) {
+    double t = 1.0;
+
+    int size = schedule.schedule.length;
+
+    if(size < 100) {
+      t = size * 10.0;
+    }
+    if(size < 1000) {
+      t = size * 3.0;
+    }
+    if(size < 10000) {
+      t = size * 0.5;
+    }
+
+    return t;
+  }
+
+  /**
+   * This method creates a new solution given a current solution, a list of already 
+   * assigned courses, and a problem. This new solution is a 'move' for the 
+   * simulated annealing algorithm
+   * 
+   * @param currentSolution
+   * @param assignment
+   * @param problem
+   * @return
+   */
   Schedule move(Schedule currentSolution, boolean[] assignment, SchedulingProblem problem) {
     Schedule newSolution = createCopy(currentSolution, problem);
-    int newCourseIndex = pickRandomCourse(problem, assignment);
-    Course c = problem.courses.get(newCourseIndex);
+    int[][] s = newSolution.schedule;
+    Random random = new Random();
 
-    for(int i = 0; i < c.timeSlotValues.length; i++) {
-      for(int j = 0; j < problem.rooms.size(); j++) {
-        if(newSolution.schedule[j][i] > -1) {
-          newSolution.schedule[j][i] = newCourseIndex;
-          return newSolution;
-        }
-      }
-    }
-    return null;
+    int firstX = random.nextInt(s.length);
+    int firstY = random.nextInt(s[0].length);
+    int secondX = random.nextInt(s.length);
+    int secondY = random.nextInt(s[0].length);
+
+    int temp = s[firstX][firstY];
+    s[firstX][firstY] = s[secondX][secondY];
+    s[secondX][secondY] =  temp;
+
+    return newSolution;
   }
 
-  // Schedule move(Schedule currentSolution, boolean[] assignment, SchedulingProblem problem) {
-  //   Schedule newSolution = createCopy(currentSolution, problem);
-
-  //   int firstCourse = -1;
-  //   int secondCourse = -1;
-  //   int firstTimeSlot = -1;
-  //   int secondTimeSlot = -1;
-
-  //   for(int i = 0; i < newSolution.schedule.length; i++) {        // Rows = Rooms
-  //     for(int j = 0; j < newSolution.schedule[i].length; ++j) {   // Columns = Timeslots
-  //       if (firstCourse == -1 && newSolution.schedule[i][j] > -1) {
-  //         firstCourse = newSolution.schedule[i][j];
-  //         firstTimeSlot = j;
-  //       } 
-  //       if(secondCourse == -1 && newSolution.schedule[i][j] > -1) {
-  //         secondCourse = newSolution.schedule[i][j];
-  //         secondTimeSlot = j;
-  //       }
-
-  //       if(firstCourse > -1 && secondCourse > -1) {
-  //         double firstBonus = problem.courses.get(firstCourse).timeSlotValues[firstTimeSlot];
-  //         double secondBonus = problem.courses.get(secondCourse).timeSlotValues[secondTimeSlot];
-  //         double actualBonus = firstBonus + secondBonus;
-
-  //         double newFBonus = problem.courses.get(firstCourse).timeSlotValues[secondTimeSlot];
-  //         double newSBonus = problem.courses.get(secondCourse).timeSlotValues[firstTimeSlot];
-  //         double newBonus = newFBonus + newSBonus;
-
-  //         if(actualBonus < newBonus) {
-  //           newSolution.schedule[i][firstTimeSlot] = secondCourse;
-  //           newSolution.schedule[i][secondTimeSlot] = firstCourse;
-  //           return newSolution;
-  //         } else {
-  //           firstCourse = -1;
-  //           secondCourse = -1;
-  //           firstTimeSlot = -1;
-  //           secondTimeSlot = -1;
-  //         }
-  //       }
-  //     }
-  //   }
-
-  //   return newSolution;
-  // }
-
+  /**
+   * This method creates a copy of a schedule given a problem
+   * 
+   * @param current
+   * @param problem
+   * @return
+   */
   Schedule createCopy(Schedule current, SchedulingProblem problem) {
     Schedule copy = problem.getEmptySchedule();
 
@@ -124,6 +159,13 @@ public class SearchAlgorithm {
     return copy;
   }
 
+  /**
+   * This method checks if exp probability is met given
+   * a delta energy and some temperature
+   * 
+   * @param deltaE
+   * @param t
+   */
   boolean probabilityMet(double deltaE, double t) {
     Random random = new Random();
     double prob = random.nextDouble() * 1;
@@ -132,34 +174,37 @@ public class SearchAlgorithm {
     return Math.exp(power) > prob ? true : false;
   }
 
-  Schedule initialSchedule(SchedulingProblem problem, Schedule solution, boolean[] assignment) {
+  /**
+   * This algorithm generates an initial random solution for simulated 
+   * annealing algorithm.
+   * 
+   * @param problem
+   * @param solution
+   * @return
+   */
+  Schedule initialSchedule(SchedulingProblem problem, Schedule solution) {
     Random random = new Random();
 
-    int courseIndex = random.nextInt(problem.courses.size());;
-    Course c = problem.courses.get(courseIndex);
+    for(int i = 0; i < problem.courses.size(); i++) {
+      int randomTimeSlot = random.nextInt(problem.courses.get(i).timeSlotValues.length);
+      int randomRoom =  random.nextInt(problem.rooms.size());
 
-    for(int i = 0; i < c.timeSlotValues.length; i++){
-      for(int j = 0; j < problem.rooms.size(); j++) {
-        if (solution.schedule[j][i] < 0) {
-            while(!assignment[courseIndex]) {
-              courseIndex = random.nextInt(problem.courses.size());
-              c = problem.courses.get(courseIndex);
-              System.out.println("I am here!");
-            }
-            solution.schedule[j][i] = courseIndex;
-            assignment[courseIndex] = true;
-        }
-      }
+      solution.schedule[randomRoom][randomTimeSlot] = i;
     }
 
     return solution;
   }
 
-  // Your search algorithm should return a solution in the form of a valid
-  // schedule before the deadline given (deadline is given by system time in ms)
+  /**
+   * This method allows to implement backtracking algorithm given a problem
+   * and a deadline
+   * 
+   * @param problem
+   * @param deadline
+   * @return
+   */
   public Schedule backtrackAlg(SchedulingProblem problem, long deadline) {
 
-    // get an empty solution to start from
     Schedule solution = problem.getEmptySchedule();
     boolean[] assignment = new boolean[problem.courses.size()];
     int[] courseConstraints = genContraintsList(problem);
@@ -169,10 +214,21 @@ public class SearchAlgorithm {
     return solution;
   }
 
+  /**
+   * This method implements backtracking search algorithm using least
+   * constraining value as heuristic. This algorithm requires a problem, a list of 
+   * already assigned courses, a schedule, and a list of constrains for each course.
+   * 
+   * @param assignment
+   * @param problem
+   * @param solution
+   * @param courseConstraints
+   */
   public Schedule recursiveBacktracking(boolean[] assignment, SchedulingProblem problem, Schedule solution, int[] courseConstraints) {
     if(isComplete(assignment)) return solution;
 
     Course c = leastConstrainingCourse(assignment, problem, courseConstraints);
+    // Course c =  selectUnassignedCourse(assignment, problem);
     boolean preferredEnabled = true;
 
     for(int i = 0; i < c.timeSlotValues.length; i ++) {
@@ -204,6 +260,15 @@ public class SearchAlgorithm {
     return solution;
   }
 
+  /**
+   * This method returns the least constraining course given a problem,
+   * a list of constraints of all courses, and a list of which courses have been
+   * already assigned
+   * 
+   * @param assignment
+   * @param problemn
+   * @param courseConstraints
+   */
   Course leastConstrainingCourse(boolean[] assignment, SchedulingProblem problem, int[] courseConstraints) {
     Course leastConstrainingCourse = null;
     int remainingConstraints = 0;
@@ -226,6 +291,15 @@ public class SearchAlgorithm {
     return leastConstrainingCourse;
   }
 
+  /**
+   * This method sums up all the constraints available in the list of 
+   * constraints and returns such a number
+   * 
+   * @param constraints
+   * @param selectedCourse
+   * @param problem
+   * @return
+   */
   int remainingConstraints(int[] constraints, Course selectedCourse, SchedulingProblem problem) {
     int sumConstraints = 0;
 
@@ -241,6 +315,14 @@ public class SearchAlgorithm {
     return sumConstraints;
   }
 
+  /**
+   * This method updates the list of constraints for every course depending 
+   * on the selected course and the given problem
+   * 
+   * @param constraints
+   * @param selectedCourse
+   * @param problem
+   */
   void updateConstraints (int[]constraints, Course selectedCourse, SchedulingProblem problem) {
     for(int i = 0; i < problem.courses.size(); i++) {
       Course currentCourse = problem.courses.get(i);
@@ -252,6 +334,13 @@ public class SearchAlgorithm {
     }
   }
 
+/**
+ * Given a problem this method generates a list counting the 
+ * constraints for each course
+ * 
+ * @param problem
+ * @return
+ */
   int[] genContraintsList(SchedulingProblem problem) {
     int[] courseConstraints = new int[problem.courses.size()];
 
@@ -266,6 +355,13 @@ public class SearchAlgorithm {
     return courseConstraints;
   }
 
+  /**
+   * This method checks whether the list of assigned courses is
+   * completed
+   * 
+   * @param assignment
+   * @return
+   */
   boolean isComplete(boolean[] assignment) {
     for(int i = 0; i < assignment.length; i++) {
       if (assignment[i] == false)
@@ -274,6 +370,14 @@ public class SearchAlgorithm {
     return true;
   }
 
+  /**
+   * This method selects the first unassigned course and returns it given
+   * a problem and the list of already assigned courses
+   * 
+   * @param assignment
+   * @param problem
+   * @return
+   */
   Course selectUnassignedCourse(boolean[] assignment, SchedulingProblem problem) {
     for(int i = 0; i < assignment.length; i++) {
       if(assignment[i] == false)
